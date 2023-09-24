852b3da447263487580635855f2d4f46836f1943
MargoShaw <2923255006@qq.com>
2023-09-24 18:21:46 +0800

    图片回显bug修复

--- blog-api/src/main/java/com/zhu/blog/controller/UploadController.java
+++ blog-api/src/main/java/com/zhu/blog/controller/UploadController.java
@@ -1,13 +1,7 @@
 package com.zhu.blog.controller;
 
-import com.zhu.blog.utils.QiniuUtils;
 import com.zhu.blog.vo.Result;
-import com.qiniu.common.QiniuException;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
 import java.util.UUID;
 
 @RestController
@@ -48,7 +39,7 @@ public class UploadController {
             file.transferTo(dest);
 
             // 构建文件的相对路径（可根据需要修改为绝对路径）
-            String relativeFilePath = "http://172.17.0.4:8888/api/images/" + newFileName;
+            String relativeFilePath = "http://111.229.247.36/static/images/" + newFileName;
 
             // 返回文件的路径给前端
             return Result.success(relativeFilePath);
